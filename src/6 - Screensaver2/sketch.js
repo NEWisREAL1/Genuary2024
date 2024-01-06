c73d29e47e0c7dc6821470b2865b69c0e331b5bf
NEWisREAL <116555607+NEWisREAL1@users.noreply.github.com>
2024-01-06 22:22:43 +0700

    prompt 6 - 2
    Screensaver again QwQ

---
+++ src/6 - Screensaver2/sketch.js
@@ -0,0 +1,47 @@
+let w = 30;
+let gridW, gridH;
+let rows, cols;
+let zOff = 0;
+let yOff = 0;
+
+function setup() {
+    createCanvas(windowWidth, windowHeight, WEBGL);
+
+    gridW = max(width, height);
+    gridH = max(width, height);
+    cols = 1.75*gridW / w;
+    rows = gridH / w;
+
+    //saveGif('genuary2024-6_2.gif', 5);
+}
+
+function draw() {
+    colorMode(RGB);
+    background(0, 71, 100);
+    rotateX(PI/2.5);
+    translate(-1.75*gridW/2, -gridH/2);
+
+    stroke(0, 0, 255);
+    colorMode(HSL);
+    //noStroke();
+    for (let i = 0; i < cols; i++) {
+        for (let j = 0; j < rows; j++) {
+            let n = noise(i*0.05, j*0.05 + yOff, zOff);
+            push();
+
+            translate(i*w, j*w);
+            fill(map(n, 0, 1, 110, 250), 75, 60);
+            box(w, w, floor(map(n, 0, 1, 1, 350)));
+
+            pop();
+        }
+    }
+
+    zOff += 0.05;
+    yOff -= 0.02;
+    //noLoop();
+}
+
+function windowResized() {
+    resizeCanvas(windowWidth, windowHeight);
+}
