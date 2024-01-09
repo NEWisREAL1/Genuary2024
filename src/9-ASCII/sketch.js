6d094dcc0a49bc71c5285e439890d67473bed11c
NEWisREAL <116555607+NEWisREAL1@users.noreply.github.com>
2024-01-09 22:12:07 +0700

    prompt 9+

--- src/9-ASCII/sketch.js
+++ src/9-ASCII/sketch.js
@@ -1,6 +1,6 @@
-const charSet = "$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\|()1{}[]?-_+~<>i!lI;:,\"^`'. ";
+const charSet = "$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft";
 let cam;
-let downScl = 10;
+let downScl = 16;
 let w, h;
 let offSet = 0;
 
@@ -8,6 +8,7 @@ function setup() {
     createCanvas(windowWidth, windowHeight);
 
     cam = createCapture(VIDEO);
+    //cam = createVideo('goldfish.mp4');
     cam.size(width/downScl, height/downScl);
     cam.hide();
 
@@ -16,28 +17,25 @@ function setup() {
 
     pixelDensity(1);
     noStroke();
-    textSize(w);
+    textSize(1.25*w);
 }
 
 function draw() {
-    background(50);
+    background(25);
     cam.loadPixels();
     for (let y = 0; y < cam.height; y++) {
         for (let x = 0; x < cam.width; x++) {
             let i = (x + y * cam.width) * 4;
             let brightness = (cam.pixels[i] + cam.pixels[i+1] + cam.pixels[i+2]) / 3;
-            let bChar = charSet[(round(map(brightness, 255, 0, 0, charSet.length - 1)))];
-            //fill(cam.pixels[i], cam.pixels[i+1], cam.pixels[i+2]);
+            let bChar = charSet[(round(map(brightness, 0, 255, 0, charSet.length - 1)))];
             fill(cam.pixels[i], cam.pixels[i+1], cam.pixels[i+2]);
             text(bChar, width - x * w, y * h + h);
         }
     }
-
-    offSet++;
 }
 
 function mouseClicked() {
-    cam.play();
+    //cam.play();
 }
 
 function windowResized() {
